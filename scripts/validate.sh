b088e07bdc924a52dc0f0371bd678883099ad524
Kaito Ii <kaitoii1111@gmail.com>
2022-07-18 22:37:01 +0900

    fix path

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -17,7 +17,7 @@ done
 kubeconform_config=( "-strict" "-ignore-missing-schemas" "-schema-location" "default" "-schema-location" "/tmp/flux-crd-schemas" "-verbose")
 
 echo "INFO - Validating clusters"
-find ./clusters -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
+find ./mycluster -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
   do
     kubeconform "${kubeconform_config[@]}" "${file}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
